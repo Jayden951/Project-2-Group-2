76c23627f014c074dd8d92dffba2eb6b65553ecc
Craig <80527795+CraigJarvis@users.noreply.github.com>
2021-08-27 14:41:27 +0200

    Adding my file

--- MatchingSkills.java
+++ MatchingSkills.java
@@ -10,29 +10,118 @@ import java.util.*;
 
 public class MatchingSkills {
     
-    private static void FindCommonSkill(String[] arr1,String[] arr2) {
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
+    private static Set<String> FindCommonSkill(ArrayList<String> arr1,ArrayList<String> arr2) {
+        Set<String> set = new HashSet<>(); //inialize "set"
+        
+        //loop through 2 ArrayLists
+        for (int i = 0; i < arr1.size(); i++) {
+            for (int j = 0; j < arr2.size(); j++) {
                 //comparing strings of arr1 and arr2 without case sensitivity
-                if (arr1[i].equalsIgnoreCase(arr2[j])) {
-                    // adding the common elements to the array "set"
-                    set.add(arr1[i]);
+                if (arr1.get(i).equalsIgnoreCase(arr2.get(j))) {
+                    //adding the common elements to the array "set"
+                    set.add(arr1.get(i));
                     break;
                 }
             }
         }
-        for (String i : set) {
-            System.out.println(i);
-        }
+        return set;
     }
     
     public static void main(String[] args) {
+        //initializing arrays
+        ArrayList<String> required_skills = new ArrayList<String>(); //skills required to get the job
+        required_skills.add("java");
+        required_skills.add("html");
+        required_skills.add("problem solving");
+        required_skills.add("Communication");
+        
+        ArrayList<String> aquired_skills = new ArrayList<String>(); //skills required to get the job
+        aquired_skills.add("java");
+        aquired_skills.add("communication");
+        aquired_skills.add("HTML");
+        aquired_skills.add("Communication");
+         
+        
+        // Printing common elements
+        System.out.println(FindCommonSkill(required_skills, aquired_skills));
+    }
+}
+
+
+
+
+/**
+ *
+ * @author Craig Jarvis
+ */
+
+/*
+package matchingskills;
+
+import java.io.*;
+import java.util.*;
+
+public class MatchingSkills {
+
+*/
+    
+    /*private static Set<String> FindCommonSkill(ArrayList<String> arr1,ArrayList<String> arr2) {
+        Set<String> set = new HashSet<>(); //inialize "set"
+        
+        //loop through 2 ArrayLists
+        for (int i = 0; i < arr1.size(); i++) {
+            for (int j = 0; j < arr2.size(); j++) {
+                //comparing strings of arr1 and arr2 without case sensitivity
+                if (arr1.get(i).equalsIgnoreCase(arr2.get(j))) {
+                    //adding the common elements to the array "set"
+                    set.add(arr1.get(i));
+                    break;
+                }
+            }
+        }
+        return set;
+    }*/
+    
+    /*
+    public static void main(String[] args) {
+        //initializing arrays
+        ArrayList<String> required_skills = new ArrayList<String>(); //skills required to get the job
+        required_skills.add("java");
+        required_skills.add("html");
+        required_skills.add("problem solving");
+        required_skills.add("Communication");
+        
+        ArrayList<String> aquired_skills = new ArrayList<String>(); //skills required to get the job
+        aquired_skills.add("java");
+        aquired_skills.add("communication");
+        aquired_skills.add("HTML");
+        aquired_skills.add("Communication");
+        
+        Collections.sort(required_skills); 
+        Collections.sort(aquired_skills); 
+        
+        required_skills.retainAll(aquired_skills);
+        System.out.println(required_skills);
+        
+        // Printing common elements
+        //System.out.println(FindCommonSkill(required_skills, aquired_skills));
+    }
+}
+
+
+if (arr1[i].equalsIgnoreCase(arr2[j]))
+
+
+public static void main(String[] args) {
         //initializing arrays
         String[] required_skills = {"java", "html", "problem solving", "Communication"}; //skills required to get the job
         String[] aquired_skills = {"java", "communication", "HTML"}; //skills job applicant has selected   
         
         // Printing common elements
         FindCommonSkill(required_skills, aquired_skills);
-    }   
-}
+
+
+for (String i : set) {
+            System.out.println(i);
+        }
+*/
